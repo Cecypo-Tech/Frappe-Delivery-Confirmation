0b53ee890449a1f81e9fee9ab51f031fcfe95c5c
Cecypo.Tech <support@cecypo.tech>
2026-03-18 15:29:39 +0300

    feat: Initialize App

---
+++ README.md
@@ -0,0 +1,33 @@
+### Delivery Confirmation
+
+Confirm delivery status using a QR code
+
+### Installation
+
+You can install this app using the [bench](https://github.com/frappe/bench) CLI:
+
+```bash
+cd $PATH_TO_YOUR_BENCH
+bench get-app $URL_OF_THIS_REPO --branch version-16
+bench install-app cecypo_delivery_confirmation
+```
+
+### Contributing
+
+This app uses `pre-commit` for code formatting and linting. Please [install pre-commit](https://pre-commit.com/#installation) and enable it for this repository:
+
+```bash
+cd apps/cecypo_delivery_confirmation
+pre-commit install
+```
+
+Pre-commit is configured to use the following tools for checking and formatting your code:
+
+- ruff
+- eslint
+- prettier
+- pyupgrade
+
+### License
+
+mit
---
+++ cecypo_delivery_confirmation/hooks.py
@@ -0,0 +1,252 @@
+app_name = "cecypo_delivery_confirmation"
+app_title = "Delivery Confirmation"
+app_publisher = "Cecypo.Tech"
+app_description = "Confirm delivery status using a QR code"
+app_email = "support@cecypo.tech"
+app_license = "mit"
+
+# Apps
+# ------------------
+
+# required_apps = []
+
+# Each item in the list will be shown as an app in the apps page
+# add_to_apps_screen = [
+# 	{
+# 		"name": "cecypo_delivery_confirmation",
+# 		"logo": "/assets/cecypo_delivery_confirmation/logo.png",
+# 		"title": "Delivery Confirmation",
+# 		"route": "/cecypo_delivery_confirmation",
+# 		"has_permission": "cecypo_delivery_confirmation.api.permission.has_app_permission"
+# 	}
+# ]
+
+# Includes in <head>
+# ------------------
+
+# include js, css files in header of desk.html
+# app_include_css = "/assets/cecypo_delivery_confirmation/css/cecypo_delivery_confirmation.css"
+# app_include_js = "/assets/cecypo_delivery_confirmation/js/cecypo_delivery_confirmation.js"
+
+# include js, css files in header of web template
+# web_include_css = "/assets/cecypo_delivery_confirmation/css/cecypo_delivery_confirmation.css"
+# web_include_js = "/assets/cecypo_delivery_confirmation/js/cecypo_delivery_confirmation.js"
+
+# include custom scss in every website theme (without file extension ".scss")
+# website_theme_scss = "cecypo_delivery_confirmation/public/scss/website"
+
+# include js, css files in header of web form
+# webform_include_js = {"doctype": "public/js/doctype.js"}
+# webform_include_css = {"doctype": "public/css/doctype.css"}
+
+# include js in page
+# page_js = {"page" : "public/js/file.js"}
+
+# include js in doctype views
+# doctype_js = {"doctype" : "public/js/doctype.js"}
+# doctype_list_js = {"doctype" : "public/js/doctype_list.js"}
+# doctype_tree_js = {"doctype" : "public/js/doctype_tree.js"}
+# doctype_calendar_js = {"doctype" : "public/js/doctype_calendar.js"}
+
+# Svg Icons
+# ------------------
+# include app icons in desk
+# app_include_icons = "cecypo_delivery_confirmation/public/icons.svg"
+
+# Home Pages
+# ----------
+
+# application home page (will override Website Settings)
+# home_page = "login"
+
+# website user home page (by Role)
+# role_home_page = {
+# 	"Role": "home_page"
+# }
+
+# Generators
+# ----------
+
+# automatically create page for each record of this doctype
+# website_generators = ["Web Page"]
+
+# automatically load and sync documents of this doctype from downstream apps
+# importable_doctypes = [doctype_1]
+
+# Jinja
+# ----------
+
+# add methods and filters to jinja environment
+# jinja = {
+# 	"methods": "cecypo_delivery_confirmation.utils.jinja_methods",
+# 	"filters": "cecypo_delivery_confirmation.utils.jinja_filters"
+# }
+
+# Installation
+# ------------
+
+# before_install = "cecypo_delivery_confirmation.install.before_install"
+# after_install = "cecypo_delivery_confirmation.install.after_install"
+
+# Uninstallation
+# ------------
+
+# before_uninstall = "cecypo_delivery_confirmation.uninstall.before_uninstall"
+# after_uninstall = "cecypo_delivery_confirmation.uninstall.after_uninstall"
+
+# Integration Setup
+# ------------------
+# To set up dependencies/integrations with other apps
+# Name of the app being installed is passed as an argument
+
+# before_app_install = "cecypo_delivery_confirmation.utils.before_app_install"
+# after_app_install = "cecypo_delivery_confirmation.utils.after_app_install"
+
+# Integration Cleanup
+# -------------------
+# To clean up dependencies/integrations with other apps
+# Name of the app being uninstalled is passed as an argument
+
+# before_app_uninstall = "cecypo_delivery_confirmation.utils.before_app_uninstall"
+# after_app_uninstall = "cecypo_delivery_confirmation.utils.after_app_uninstall"
+
+# Desk Notifications
+# ------------------
+# See frappe.core.notifications.get_notification_config
+
+# notification_config = "cecypo_delivery_confirmation.notifications.get_notification_config"
+
+# Permissions
+# -----------
+# Permissions evaluated in scripted ways
+
+# permission_query_conditions = {
+# 	"Event": "frappe.desk.doctype.event.event.get_permission_query_conditions",
+# }
+#
+# has_permission = {
+# 	"Event": "frappe.desk.doctype.event.event.has_permission",
+# }
+
+# Document Events
+# ---------------
+# Hook on document methods and events
+
+# doc_events = {
+# 	"*": {
+# 		"on_update": "method",
+# 		"on_cancel": "method",
+# 		"on_trash": "method"
+# 	}
+# }
+
+# Scheduled Tasks
+# ---------------
+
+# scheduler_events = {
+# 	"all": [
+# 		"cecypo_delivery_confirmation.tasks.all"
+# 	],
+# 	"daily": [
+# 		"cecypo_delivery_confirmation.tasks.daily"
+# 	],
+# 	"hourly": [
+# 		"cecypo_delivery_confirmation.tasks.hourly"
+# 	],
+# 	"weekly": [
+# 		"cecypo_delivery_confirmation.tasks.weekly"
+# 	],
+# 	"monthly": [
+# 		"cecypo_delivery_confirmation.tasks.monthly"
+# 	],
+# }
+
+# Testing
+# -------
+
+# before_tests = "cecypo_delivery_confirmation.install.before_tests"
+
+# Extend DocType Class
+# ------------------------------
+#
+# Specify custom mixins to extend the standard doctype controller.
+# extend_doctype_class = {
+# 	"Task": "cecypo_delivery_confirmation.custom.task.CustomTaskMixin"
+# }
+
+# Overriding Methods
+# ------------------------------
+#
+# override_whitelisted_methods = {
+# 	"frappe.desk.doctype.event.event.get_events": "cecypo_delivery_confirmation.event.get_events"
+# }
+#
+# each overriding function accepts a `data` argument;
+# generated from the base implementation of the doctype dashboard,
+# along with any modifications made in other Frappe apps
+# override_doctype_dashboards = {
+# 	"Task": "cecypo_delivery_confirmation.task.get_dashboard_data"
+# }
+
+# exempt linked doctypes from being automatically cancelled
+#
+# auto_cancel_exempted_doctypes = ["Auto Repeat"]
+
+# Ignore links to specified DocTypes when deleting documents
+# -----------------------------------------------------------
+
+# ignore_links_on_delete = ["Communication", "ToDo"]
+
+# Request Events
+# ----------------
+# before_request = ["cecypo_delivery_confirmation.utils.before_request"]
+# after_request = ["cecypo_delivery_confirmation.utils.after_request"]
+
+# Job Events
+# ----------
+# before_job = ["cecypo_delivery_confirmation.utils.before_job"]
+# after_job = ["cecypo_delivery_confirmation.utils.after_job"]
+
+# User Data Protection
+# --------------------
+
+# user_data_fields = [
+# 	{
+# 		"doctype": "{doctype_1}",
+# 		"filter_by": "{filter_by}",
+# 		"redact_fields": ["{field_1}", "{field_2}"],
+# 		"partial": 1,
+# 	},
+# 	{
+# 		"doctype": "{doctype_2}",
+# 		"filter_by": "{filter_by}",
+# 		"partial": 1,
+# 	},
+# 	{
+# 		"doctype": "{doctype_3}",
+# 		"strict": False,
+# 	},
+# 	{
+# 		"doctype": "{doctype_4}"
+# 	}
+# ]
+
+# Authentication and authorization
+# --------------------------------
+
+# auth_hooks = [
+# 	"cecypo_delivery_confirmation.auth.validate"
+# ]
+
+# Automatically update python controller files with type annotations for this app.
+# export_python_type_annotations = True
+
+# default_log_clearing_doctypes = {
+# 	"Logging DocType Name": 30  # days to retain logs
+# }
+
+# Translation
+# ------------
+# List of apps whose translatable strings should be excluded from this app's translations.
+# ignore_translatable_strings_from = []
+
